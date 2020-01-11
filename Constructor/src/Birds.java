98308bf9dd17d03e00c5ce3fe981b69262a86263
'UTMarckus' <'utmarckus@gmail.com'>
2020-01-11 13:15:19 +0200

    v0.1

---
+++ Constructor/src/Birds.java
@@ -0,0 +1,18 @@
+public class Birds {
+	
+	private String name;
+	private boolean canFly;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isCanFly() {
+		return canFly;
+	}
+	public void setCanFly(boolean canFly) {
+		this.canFly = canFly;
+	}
+}
--- Constructor/src/Main.java
+++ Constructor/src/Main.java
@@ -2,7 +2,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Birds bird = new Birds();
 		
+		bird.setName("qwer");
+		bird.setCanFly(true);
+		
+		System.out.println(bird.getName() + "\n" + bird.isCanFly());
 
 	}
 
