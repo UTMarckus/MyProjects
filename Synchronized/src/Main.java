d60e9b843a107105371d6d8bcd01b6702f1965a1
'UTMarckus' <'utmarckus@gmail.com'>
2020-01-11 09:59:21 +0200

    Synchronized v1.0

--- Synchronized/src/Main.java
+++ Synchronized/src/Main.java
@@ -1,9 +1,28 @@
-
 public class Main {
+	
+	private static int number;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Thread t1 = new Thread(new Worker());
+		Thread t2 = new Thread(new Worker());
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(number);
 
+	}
+	
+	public static synchronized void increceNumber() {
+		number++;
 	}
 
 }
