2c55f6475c8694f1cc018e2b64a8c281c7900b7a
'UTMarckus' <'utmarckus@gmail.com'>
2020-01-11 12:08:03 +0200

    v0.1

--- Array Blocking Queue/src/Main.java
+++ Array Blocking Queue/src/Main.java
@@ -1,9 +1,35 @@
+import java.util.ArrayList;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		
+	private static ArrayList<Object> list = new ArrayList<Object>();
 
+	public static void main(String[] args) {
+
+		addObject();
+		proceed();
+
+	}
+
+	private static void addObject() {
+		for (int i = 0; i < 5; i++) {
+			list.add(new Object());
+		}
+	}
+
+	private static void proceed() {
+		System.out.println(list.size());
+		
+		while(list.size()!=0) {
+			Object obj = new Object();
+			for (Object el : list) {
+				obj = el;
+				break;
+			}
+			list.remove(obj);
+		}
+		
+		System.out.println(list.size());
 	}
 
 }
